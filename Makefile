321484540e72e5af3acccc2134eab8a707d5dee9
grenaud- <grenaud-@student.42lausanne.ch>
2022-04-27 16:14:53 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: grenaud- <grenaud-@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/04/26 18:26:23 by grenaud-          #+#    #+#              #
+#    Updated: 2022/04/27 16:02:04 by grenaud-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SOURCES = server.c client.c
+OBJECTS = $(SOURCES:.c=.o)
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+all: server client
+
+bonus: server client
+
+server: server.o libft
+	$(CC) -o $@ $< -Llibft -lft
+
+client: client.o libft
+	$(CC) -o $@ $< -Llibft -lft
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $?
+
+libft:
+	make -C libft
+
+clean:
+	rm -f $(OBJECTS)
+	make -C libft clean
+	
+fclean: clean
+	rm -f server client libft/libft.a
+
+re: fclean all
+
+.PHONY: all bonus libft clean fclean re
---
+++ libft/strjoin_gnl.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_gnl.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grenaud- <grenaud-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 16:31:29 by marvin            #+#    #+#             */
+/*   Updated: 2022/03/23 14:52:33 by grenaud-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strjoin_gnl(char *left_str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!left_str)
+	{
+		left_str = (char *)malloc(1 * sizeof(char));
+		left_str[0] = '\0';
+	}
+	if (!left_str || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (left_str)
+		while (left_str[++i] != '\0')
+			str[i] = left_str[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
+	free(left_str);
+	return (str);
+}
